ba30e7b2c05ef622081bb0ca02de7eba2fa5c82f
John Tram <john.tram@pm.me>
2023-11-12 11:01:15 -0500

    update logic and re-arange code

--- nrg_status.sh
+++ nrg_status.sh
@@ -3,22 +3,30 @@ export PATH=$PATH:$HOME/energi3/bin
 #original Author: @ProjectJourneyman
 
 #updated to use sendgrid
-#the following env variables defined:
+#the following env variables defined must be defined and set:
 #   SENDGRID_API_KEY
 #   TO_EMAIL_ADDR
 #   FROM_EMAIL_ADDR 
+#   NRG_ADDR
 
-#isRunning=$(energi3 --exec "masternode.masternodeInfo('MyAddress')" attach 2>/dev/null | grep -Fq "isActive: true" && echo $?)
+echo "From Addr" ${FROM_EMAIL_ADDR}
+
+isRunning=$(energi3 --exec "masternode.masternodeInfo('${NRG_ADDR}')" attach 2>/dev/null | grep -Fq "isActive: true" && echo $?)
 
 isStaking=$(energi3 --exec "miner.stakingStatus()" attach 2>/dev/null | grep -Fq "staking: true" && echo $?)
 
-msg="NOT RUNNING"
-subj="NRG Staking Status"
+echo "isRunning is: " ${isRunning}
+echo "isStaking is: " ${isStaking}
 
-if [[ $isRunning == 0 && $isStaking == 0 ]]; then
+msg="NOT RUNNING"
+subj="NRG Staking Status from GCP"
+
+if [[ -n $isRunning && -n $isStaking ]]; then
         msg="running and staking"
-elif [[ $isStaking == 0 ]]; then
-    msg="staking but masternode not active"
+elif [[ -n $isRunning ]]; then
+        msg="running but not staking - check NRG balance"
+elif [[ -n $isStaking ]]; then
+        msg="staking but masternode not active"
 fi
 
 if [[ $msg == "NOT RUNNING" ]]; then
@@ -29,7 +37,7 @@ echo $msg
 echo $subj
 
 #send notification
-email_data='{"personalizations": [{"to": [{"email": "'${TO_EMAIL_ADDR}'"}]}],"from": {"email": "'${FROM_EMAIL_ADDR}'"},"subject": "'${subj}'","content": [{"type": "text/plain", "value":"'${msg}'"}]}'
+email_data='{"personalizations": [{"to": [{"email": "'${TO_EMAIL_ADDR}'"}]}],"from": {"email": "'${FROM_EMAIL_ADDR}'"},"subject": "'${subj}'","content": [{"type": "text/plain", "value":"'${msg}'>
 
 curl --request POST \
     --url https://api.sendgrid.com/v3/mail/send \
